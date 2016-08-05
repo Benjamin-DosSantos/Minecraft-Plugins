642696725fbeaaa905b967fac66aa90b169cb6af
Benjamin DosSantos <BlackPenSoftware@Gmail.com>
2016-08-05 11:51:46 -0400

    Hot fix for roman numerals

--- ItemInfo/src/MainCommands/Item.java
+++ ItemInfo/src/MainCommands/Item.java
@@ -81,19 +81,22 @@ public class Item extends Command{
 		return true;
 	}
 	
-	private String romans(Integer value) {
-		switch (value){
-			default:
-				return "I";
-			case 2:
-				return "II";
-			case 3:
-				return "III";
-			case 4:
-				return "IV";
-			case 5:
-				return "V";
-		}
+	public static String romans(int num) {
+	    StringBuilder sb = new StringBuilder();
+	    int times = 0;
+	    String[] romans = new String[] { "I", "IV", "V", "IX", "X", "XL", "L",
+	            "XC", "C", "CD", "D", "CM", "M" };
+	    int[] ints = new int[] { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500,
+	            900, 1000 };
+	    for (int i = ints.length - 1; i >= 0; i--) {
+	        times = num / ints[i];
+	        num %= ints[i];
+	        while (times > 0) {
+	            sb.append(romans[i]);
+	            times--;
+	        }
+	    }
+	    return sb.toString();
 	}
 
 	public static String realName(String currentName){
